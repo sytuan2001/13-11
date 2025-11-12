9c712f1f56d8c4fa6f36b2853eefd44a82f07392
sytuan2001 <thuancat1978@gmail.com>
2025-11-12 09:43:35 +0700

    add 13/11

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log('Happy developing ✨')
+let imgObj = null;
+imgObj = document.getElementById('myImage');
+function init() {
+    imgObj = document.getElementById('myImage');
+    imgObj.style.position = 'relative';
+    imgObj.style.left = '0px';
+}
+function moveRight() {
+    imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
+}
+window.onload = init;
